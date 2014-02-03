ae94d863543a1327e68cdc2e2609c8aa98ee75dc
Donagh Horgan <donagh.horgan@gmail.com>
2014-02-03 22:22:14 +0000

    Sort by the earliest of whichever date is available.

--- albumsort.py
+++ albumsort.py
@@ -5,9 +5,35 @@ PLUGIN_VERSION = "1.0"
 PLUGIN_API_VERSIONS = ["0.12", "0.15"]
 
 from picard.metadata import register_album_metadata_processor
-import re
     
 def add_albumsort(tagger, metadata, release):
-    metadata["albumsort"] = metadata["originaldate"] + " " + metadata["album"]
+    try:
+        original = metadata["originaldate"]
+        if len(original) == 4:
+            original += "-01-01"
+        elif len(original) == 7:
+            original += "-01"
+    except:
+        original = None
+    
+    try:
+        date = metadata["date"]
+        if len(date) == 4:
+            date += "-01-01"
+        elif len(date) == 7:
+            date += "-01"
+    except:
+        date = None
+    
+    if original and date:
+        earliest = sorted([original, date])[0]
+    elif original:
+        earliest = original
+    elif date:
+        earliest = date
+    else:
+        earliest = ""
+    
+    metadata["albumsort"] = earliest + " " + metadata["album"]
 
 register_album_metadata_processor(add_albumsort)
